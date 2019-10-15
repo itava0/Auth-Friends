55ecdec377909e570ee295f182aa0f737ff5cb87
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-15 19:22:10 -0400

    created a PrivateRoute to prevent user to see if friends without login in

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 
-function App() {
+import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
+import PrivateRoute from "./components/PrivateRoute"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/FriendsList">FriendsList</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute path="/FriendsList" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
